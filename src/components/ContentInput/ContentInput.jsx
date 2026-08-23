ebf0b3e6e78c9fd2e72322dc4cf86ab51fd3be59
hiemas lerastad <hiemaslerastad@gmail.com>
2026-08-23 16:37:54 +0100

    Field value fix

--- src/components/ContentInput/ContentInput.jsx
+++ src/components/ContentInput/ContentInput.jsx
@@ -1,8 +1,8 @@
 import "./ContentInput.css"
 
-function ContentInput({tag: Tag ="input", type, onChangeFunc, onInputFunc, onFocusFunc, onBlurFunc, name, min, max, step, defaultValue, className = "", children, placeholder, value = "", checked}) {
+function ContentInput({tag: Tag ="input", type, onChangeFunc, onInputFunc, onFocusFunc, onBlurFunc, name, min, max, step, defaultValue, className = "", children, placeholder, value, checked}) {
   return (
-    <Tag className={`content-input ${ className }`} type={type} onChange={onChangeFunc} onInput={onInputFunc} onFocus={onFocusFunc} onBlur={onBlurFunc} name={name} min={min} max={max} step={step} defaultValue={defaultValue} placeholder={placeholder} value={value} checked={type === "checkbox" ? checked : false} name={name}>
+    <Tag className={`content-input ${ className }`} type={type} onChange={onChangeFunc} onInput={onInputFunc} onFocus={onFocusFunc} onBlur={onBlurFunc} name={name} min={min} max={max} step={step} defaultValue={defaultValue} placeholder={placeholder} value={value} checked={type === "checkbox" ? checked : undefined} name={name}>
       {children}
     </Tag>
   );
